b50c1f8a1d46d920b8ef74f66ea9e65ec3eb82bc
Andrew Nichols <andrew.nichols@debtsy.co>
2020-12-14 10:41:50 -0500

    Horribly inefficient star2 solution

--- 2020/11/seating.clj
+++ 2020/11/seating.clj
@@ -1,7 +1,7 @@
 (ns advent.seating
   (:require [clojure.string :as str]))
 
-(def input "11/test.dat")
+(def input "11/input.dat")
 
 (defn- text->hash-map 
   "Reads a collection of lines into a hash-map from x,y coords to the value in the block."
@@ -13,9 +13,10 @@
                             (map-indexed (fn [x cell] [[x y] (f cell)]))
                             (apply concat))))
         (apply concat)
-        (apply hash-map))))
+        (apply hash-map)
+        (assoc {:rows (count lines) :cols (count (first lines))} :map))))
 
-(defn count-adjacent [seats [x y :as pos]]
+(defn count-adjacent [{seats :map} [x y :as pos]]
   (let [adj [(seats [(dec x) (dec y)])
              (seats [(dec x) y])
              (seats [(dec x) (inc y)])
@@ -27,22 +28,58 @@
              (seats [(inc x) (inc y)])]]
     (apply + (filter some? adj))))
 
-(defn- print-seats [seats]
-  (doseq [y (range 9)]
+(defn first-seen [{seats :map} [x y :as pos] [x-func y-func]]
+  (let [cmb-fn (cond
+                 (= > x-func y-func) (fn [[x' y'] [x'' y'']] (= (- x'' x') (- y'' y')))
+                 (= < x-func y-func) (fn [[x' y'] [x'' y'']] (= (- x'' x') (- y'' y')))
+                 (and (= > x-func) (= < y-func)) (fn [[x' y'] [x'' y'']] (= (- x'' x') (- y' y'')))
+                 (and (= < x-func) (= > y-func)) (fn [[x' y'] [x'' y'']] (= (- x' x'') (- y'' y')))
+                 :else #(vector %1 %2))
+        target (filter (fn [[[x' y'] v]] (and (x-func x' x) (y-func y' y) (cmb-fn [x y] [x' y']))) seats)
+        ordered (into (sorted-map-by (fn [[x' y'] [x'' y'']] (and (x-func x'' x') (y-func y'' y')))) target)
+        filtered (filter #(not= nil (second %)) ordered)
+        ]
+    (second (first filtered))
+    )
+  )
+
+(defn count-sightline [{seats :map :as seat-map} [x y :as pos]]
+  (let [
+        adj [
+             (first-seen seat-map pos [< <])
+             (first-seen seat-map pos [< >])
+             (first-seen seat-map pos [< =])
+             (first-seen seat-map pos [= <])
+             (first-seen seat-map pos [= >])
+             (first-seen seat-map pos [> <])
+             (first-seen seat-map pos [> >])
+             (first-seen seat-map pos [> =])
+
+             ]]
+
+    ;; (println adj)
+    (apply + (filter some? adj))))
+
+
+(defn- print-seats [{seats :map :as ss}]
+  (doseq [y (range 10)]
     (doseq [x (range 10)]
       (print (case (seats [x y]) 0 "L" 1 "#" ".")))
     (print \newline)))
 
-(defn seating-round [seats]
-  (reduce (fn [acc [pos type :as seat]]
-            (let [adj (count-adjacent seats pos)]
-              (conj acc
-                    (case type
-                      nil seat
-                      1 [pos (if (>= adj 4) 0 1)]
-                      0 [pos (if (=  adj 0) 1 0)]
-                      ))))
-          {} seats))
+(defn seating-round [{count-fn :count-fn threshold :threshold} seats]
+  (assoc
+   (dissoc seats :map)
+   :map
+   (reduce (fn [acc [pos type :as seat]]
+             (let [adj (count-fn seats pos)]
+               (conj acc
+                     (case type
+                       nil seat
+                       1 [pos (if (>= adj threshold) 0 1)]
+                       0 [pos (if (=  adj 0) 1 0)]
+                       ))))
+           {} (:map seats))))
 
 (defn chaotic-iteration [opts seats]
   (loop [prev {}
@@ -52,18 +89,44 @@
       (println "Round " round)
       (print-seats curr))
     (if (= prev curr) [curr (dec round)]
-        (recur curr (seating-round curr) (inc round)))))
+        (recur curr (seating-round opts curr) (inc round)))))
 
 (def star1
   (->> input
        slurp
        str/split-lines
        (text->hash-map #(case % \. nil \L 0 1))
-       (chaotic-iteration {:print false})
+       #_(#(do (println "FOO: " %) %))
+       (chaotic-iteration {:print false :count-fn count-adjacent :threshold 4})
        first
+       :map
        (filter #(= 1 (second %)))
        count))
 
 (println star1)
 
+(def star2
+  (->> input
+       slurp
+       str/split-lines
+       (text->hash-map #(case % \. nil \L 0 1))
+       (chaotic-iteration {:print false :count-fn count-sightline :threshold 5})
+       first
+       :map
+       (filter #(= 1 (second %)))
+       count))
+
+
+(println star1 star2)
+
+#_(->> "11/test.dat"
+     slurp
+     str/split-lines
+     (text->hash-map #(case % \. nil \L 0 1))
+     (#(count-sightline % [3 0] #_[< >]))
+     #_(#(first-seen % [3 4] [> =]))
+     #_println
+     )
+
 #_(assert (= star1 2470))
+#_(assert (= star2 26))
