39e52b5aa7d4f5e988ee3361857eede0b07995c0
Andrew Nichols <andrew@frenata.net>
2024-12-05 03:01:03 +0000

    mostly working

--- 2024/2/puzzle2.py
+++ 2024/2/puzzle2.py
@@ -4,17 +4,17 @@ import functools
 import heapq
 
 
-def is_safe(line, tolerance):
-    nums = [int(x) for x in line.split()]
+def is_safe(nums, tolerance):
     dir = 1 if nums[1] > nums[0] else -1
     last = nums[0]
-    for n in nums[1:]:
+    for i, n in enumerate(nums[1:]):
+        print(nums, dir, last, n)
         if dir == 1 and n <= last:
-            return False
+            return is_safe(nums[:i]+nums[i+1:], 0) if tolerance else False
         elif dir == -1 and n >= last:
-            return False
+            return is_safe(nums[:i]+nums[i+1:], 0) if tolerance else False
         elif abs(n - last) > 3:
-            return False
+            return is_safe(nums[:i]+nums[i+1:], 0) if tolerance else False
         last = n
 
     return True
@@ -22,8 +22,13 @@ def is_safe(line, tolerance):
 
 def safe(filename, failure_tolerance=0):
     with open(filename) as f:
-        return collections.Counter([is_safe(line, failure_tolerance) for line in f]).get(True)
+        safe_count = collections.Counter(
+            [is_safe([int(x) for x in line.split()], failure_tolerance) for line in f]
+        )
+        #print(safe_count)
+        return safe_count.get(True, 0)
 
 
 if __name__ == "__main__":
     print(safe(sys.argv[1]))
+    print(safe(sys.argv[1], 1))
--- 2024/2/test_puzzle2.py
+++ 2024/2/test_puzzle2.py
@@ -3,3 +3,7 @@ import puzzle2 as sut
 def test_safe_normal():
     actual = sut.safe("2/example.txt")
     assert actual == 2
+
+def test_safe_normal():
+    actual = sut.safe("2/example.txt", 1)
+    assert actual == 4
