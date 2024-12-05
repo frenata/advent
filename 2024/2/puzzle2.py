d230584e4496abfb56621342b45b90a1d5b2b308
Andrew Nichols <andrew@frenata.net>
2024-12-05 04:19:33 +0000

    day 2 star 2

--- 2024/2/puzzle2.py
+++ 2024/2/puzzle2.py
@@ -3,30 +3,42 @@ import collections
 
 
 def is_safe(nums, tolerance):
-    # print("test", nums, tolerance)
     last = nums[0]
     dirs = []
-    if tolerance and is_safe(nums[1:], 0):
-        return True
 
+    failure = False
+
+    # First look for diff failures and collect directions
     for i, n in enumerate(nums[1:]):
         diff = abs(last - n)
         if diff < 1 or diff > 3:
-            return is_safe(nums[: i + 1] + nums[i + 2 :], 0) if tolerance else False
+            failure = True
+            break
 
         dir = 1 if n > last else (-1 if n < last else 0)
         dirs.append(dir)
         last = n
 
     dirs = collections.Counter(dirs)
-    # print(dirs)
     asc = dirs.get(1, 0)
     dsc = dirs.get(-1, 0)
 
-    if not tolerance:
-        return asc == 0 or dsc == 0
-    else:
-        return not (asc > 1 and dsc > 1)
+    if not failure:  # there still might be direction failure!
+        if asc == 0 or dsc == 0:
+            return True
+        else:
+            failure = True
+
+    if not tolerance and failure:
+        return False
+
+    # the level isn't safe, so try all possible removals
+    # annoyingly 'inefficient', but I couldn't figure out how to
+    # properly detect possible direction failure greedily
+    for i, _ in enumerate(nums):
+        if is_safe(nums[:i] + nums[i + 1 :], 0):
+            return True
+    return False
 
 
 def safe(filename, failure_tolerance=0):
@@ -34,7 +46,6 @@ def safe(filename, failure_tolerance=0):
         safe_count = collections.Counter(
             [is_safe([int(x) for x in line.split()], failure_tolerance) for line in f]
         )
-        print(safe_count)
         return safe_count.get(True, 0)
 
 
