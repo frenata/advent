accbbaa8263094eb8a9eca953a67f71afe88ccaa
Andrew Nichols <andrew@frenata.net>
2024-12-05 02:16:32 +0000

    star 2

--- 2024/1/puzzle.py
+++ 2024/1/puzzle.py
@@ -1,15 +1,18 @@
 import sys
+import collections
 import functools
 import heapq
+
 
 def split(xs, line):
     left, right = xs
     l, r = line.split()
     return (left + [int(l)], right + [int(r)])
 
+
 def distance(filename):
     with open(filename) as f:
-        left, right = functools.reduce(split, f, ([],[]))
+        left, right = functools.reduce(split, f, ([], []))
 
     heapq.heapify(left)
     heapq.heapify(right)
@@ -21,5 +24,14 @@ def distance(filename):
     return total
 
 
+def similarity(filename):
+    with open(filename) as f:
+        left, right = functools.reduce(split, f, ([], []))
+
+    right = collections.Counter(right)
+    return functools.reduce(lambda total, x: total + x * right.get(x, 0), left, 0)
+
+
 if __name__ == "__main__":
     print(distance(sys.argv[1]))
+    print(similarity(sys.argv[1]))
--- 2024/1/test_puzzle.py
+++ 2024/1/test_puzzle.py
@@ -1,5 +1,11 @@
 import puzzle
 
+
 def test_distance():
     actual = puzzle.distance("1/example.txt")
     assert actual == 11
+
+
+def test_similarity():
+    actual = puzzle.similarity("1/example.txt")
+    assert actual == 31
