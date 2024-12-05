d4a9b8ef97b95ccdc8a86c78fe8f213f423314bf
Andrew Nichols <andrew@frenata.net>
2024-12-05 04:46:47 +0000

    day 3 star 2

--- 2024/3/puzzle3.py
+++ 2024/3/puzzle3.py
@@ -3,13 +3,51 @@ import functools
 import re
 import collections
 
+
 def mult(line):
-    return [int(x)*int(y) for x,y in re.findall(r"mul\((\d+)\,(\d+)\)", line)]
+    return [int(x) * int(y) for x, y in re.findall(r"mul\((\d+)\,(\d+)\)", line)]
+
 
 def multiply(filename):
     with open(filename) as f:
-        return functools.reduce(lambda x,y: x+sum(y), [mult(line) for line in f], 0)
+        return functools.reduce(lambda x, y: x + sum(y), [mult(line) for line in f], 0)
+
+
+def extract(line):
+    commands = []
+    for x, y, enable, disable in re.findall(
+        r"mul\((\d+)\,(\d+)\)|(do)\(\)|(don)'t\(\)", line
+    ):
+        if x and y:
+            commands.append((int(x), int(y)))
+        elif enable:
+            commands.append(True)
+        elif disable:
+            commands.append(False)
+    return commands
+
+
+def conditional_multiply(filename):
+    with open(filename) as f:
+        commands = functools.reduce(
+            lambda x, y: x + y, [extract(line) for line in f], []
+        )
+
+        enabled = True
+        total = 0
+        for command in commands:
+            if command is True:
+                enabled = True
+            elif command is False:
+                enabled = False
+            elif not enabled:
+                pass
+            else:
+                total += command[0] * command[1]
+
+    return total
 
 
 if __name__ == "__main__":
     print(multiply(sys.argv[1]))
+    print(conditional_multiply(sys.argv[1]))
--- 2024/3/test_puzzle3.py
+++ 2024/3/test_puzzle3.py
@@ -1,5 +1,11 @@
 import puzzle3 as sut
 
+
 def test_multiply():
     actual = sut.multiply("3/example.txt")
     assert actual == 161
+
+
+def test_conditional_multiply():
+    actual = sut.conditional_multiply("3/example.txt")
+    assert actual == 48
