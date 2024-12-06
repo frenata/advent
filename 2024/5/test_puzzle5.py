34c997e93837f13b97937dfb2df481c30b108899
Andrew Nichols <andrew@frenata.net>
2024-12-06 12:46:21 +0000

    fix day 5 tests

--- 2024/5/test_puzzle5.py
+++ 2024/5/test_puzzle5.py
@@ -2,5 +2,8 @@ import puzzle5 as sut
 
 
 def test_order():
-    actual = sut.order("5/example.txt")
-    assert actual == 143
+    rules, pages = sut.extract("5/example.txt")
+    ordered = sut.middles([page for page in pages if sut.is_ordered(rules, page)])
+    unordered = sut.middles([sut.order(rules, page) for page in pages if not sut.is_ordered(rules, page)])
+    assert ordered == 143
+    assert unordered == 123
