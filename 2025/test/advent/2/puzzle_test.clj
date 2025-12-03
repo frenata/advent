c09bbcf7a921d62e171d26084e00c766f4d9abe5
Andrew Nichols <andrew@frenata.net>
2025-12-02 19:32:39 -0500

    fix unit test

--- 2025/test/advent/2/puzzle_test.clj
+++ 2025/test/advent/2/puzzle_test.clj
@@ -9,8 +9,7 @@
 (deftest test-star2
     (is (= 4174379265 (sut/-main "data/2/example.txt" sut/at-least-twice?))))
 
-(deftest at-least-twice?
-  (let [inputs [3 33 333 450 450450 909090909090909090 909090909]]
-    (->> inputs
-         (filter sut/at-least-twice?)
-         (= [3 33 333 450450 909090909090909090]))))
+(deftest test-at-least-twice?
+  (let [inputs [3 33 333 450 450450 909090909090909090 909090909]
+        outputs (filter sut/at-least-twice? inputs)]
+    (is (= outputs [33 333 450450 909090909090909090]))))
