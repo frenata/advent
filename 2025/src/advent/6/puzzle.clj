6005760602c8047c9fea2f26421aaf4f7ff5a82f
Andrew Nichols <andrew@frenata.net>
2025-12-07 17:53:35 -0500

    day 6 star 2

--- 2025/src/advent/6/puzzle.clj
+++ 2025/src/advent/6/puzzle.clj
@@ -10,34 +10,51 @@
     (= token "+") +
     :else (Integer/parseInt token)))
 
-(defn parse-line [row line]
+(defn parse-line-star1 [row line]
   (->> line
        str/trim
        (#(str/split % #" +"))
        (map-indexed (fn [col pos] [col [(parse-token pos)]]))))
 
-(defn parse [lines]
+(defn parse-star1 [lines]
   (->> lines
-       (map-indexed parse-line)
+       (map-indexed parse-line-star1)
        (map (partial into {}))
-       (apply merge-with into)))
+       (apply merge-with into)
+       vals))
+
+(defn parse-problem [lines [skip problems] [op size]]
+  (let [digits (->> (range size) 
+                    reverse
+                    (map (fn [n] (map (fn [line] (nth line (+ n skip))) lines) ))
+                    (map (comp Integer/parseInt str/trim #(apply str %))))]
+    [(+ skip (inc size)) (conj problems digits)]))
+
+(defn parse-star2 [lines]
+  (let [ops (->> lines
+                 last
+                 (re-seq #"(\*|\+)( +)")
+                 (map (fn [[_ op line]]
+                        [op (count line)]))
+                 ((fn [ops] (concat (drop-last ops)
+                                    [[(first (last ops)) (inc (second (last ops)))]]))))
+        problems (last (reduce (partial parse-problem (drop-last lines)) [0 []] ops)) ]
+    (map (fn [[op _] args] (concat args [(parse-token op)]) ) ops problems)))
 
 (defn calculate [problem]
   (let [op (last problem)
         ns (drop-last problem)]
-    #_(println op ns)
-    (apply op ns)
-  ))
+    (apply op ns)))
 
 (defn -main 
-  [filename]
+  ([filename]
+   (-main filename parse-star2))
+  ([filename parser]
    (with-open [rdr (io/reader filename)]
      (->> rdr
           line-seq
           util/start-timer!
-          parse
-          #_(util/spy>> "parsed")
-          vals
+          parser
           (map calculate)
           (reduce +)
-          (util/spy-timer! "output:"))))
+          (util/spy-timer! "output:")))))
--- 2025/test/advent/6/puzzle_test.clj
+++ 2025/test/advent/6/puzzle_test.clj
@@ -4,4 +4,7 @@
     [clojure.test :refer :all]))
 
 (deftest test-star1
-  (is (= 4277556 (sut/-main "data/6/example.txt"))))
+  (is (= 4277556 (sut/-main "data/6/example.txt" sut/parse-star1))))
+
+(deftest test-star2
+  (is (= 3263827 (sut/-main "data/6/example.txt" sut/parse-star2))))
